fb4ddc2db2533a338817aee7a5ad1846d78fef4f
Shanjin Yang <shanjin.yang@aispeech.com>
2016-07-18 15:38:01 +0800

    code indent and clean
    Signed-off-by: Shanjin Yang <shanjin.yang@aispeech.com>

--- fsm.c
+++ fsm.c
@@ -1,93 +1,99 @@
 #include <stdio.h>
 
-//状态的enum定义 
-enum State{ST1, ST2, ST3, ST4, ST5};
-
-//定义一个存储当前状态的结构体
-struct FsmCurState
-{
-    unsigned char curState;
+/*state name*/
+enum State {
+	ST1, ST2, ST3, ST4, ST5
 };
 
-//回调函数的指针
-typedef void (*FsmFuncPtr)(struct FsmCurState *);
-//状态表里面的表项的结构定义，回调函数和下一状态 
-struct FsmTabEle
+/*current state*/
+struct fsm_cur_state
 {
-    FsmFuncPtr ptr;
-    unsigned char nextState;        
+	unsigned char cur_state;
+};
+
+/*callback function also call action for state*/
+typedef void (*fsm_func_ptr)(struct fsm_cur_state *);
+
+/*action and next state*/
+struct fsm_table
+{
+	fsm_func_ptr ptr;
+	unsigned char nextState;        
 };
 
 
-//具体的函数 
-void doNext(struct FsmCurState *curState)
+/*state action*/
+void donext(struct fsm_cur_state *cur_state)
 {
-    printf("\n---next---\n");       
+	printf("\n---next---\n");       
 }
 
-void doNothing(struct FsmCurState *curState)
+void donothing(struct fsm_cur_state *cur_state)
 {
-    printf("\n---error---\n");      
+	printf("\n---error---\n");      
 }
 
-void doHello(struct FsmCurState *curState)
+void dohello(struct fsm_cur_state *cur_state)
 {
-    printf("\n---yes---\n");        
+	printf("\n---yes---\n");        
 }
 
 
-//状态表
-struct FsmTabEle FsmTab[5][2] = {
-    {{doNext, ST2}, 
-        {doNothing, ST1}},
+/*state table*/
+struct fsm_table fsm_tab[5][2] = {
+	{
+		{donext, ST2}, 
+		{donothing, ST1}
+	},
 
-    {{doNext, ST3}, 
-        {doNothing, ST1}},
+	{
+		{donext, ST3}, 
+		{donothing, ST1}
+	},
 
-    {{doNext, ST4}, 
-        {doNothing, ST1}},
+	{
+		{donext, ST4}, 
+		{donothing, ST1}
+	},
 
-    {{doNext, ST5}, 
-        {doNothing, ST1}},
+	{
+		{donext, ST5}, 
+		{donothing, ST1}
+	},
 
-    {{doHello, ST1},        
-        {doNothing, ST1}},
+	{
+		{dohello, ST1},        
+		{donothing, ST1}
+	},
 }; 
 
-//状态调度函数
-void FsmDispatch(struct FsmCurState *curState, unsigned char sig)
+/*state dispatch*/
+void fsm_dispatch(struct fsm_cur_state *cur_state, unsigned char sig)
 {
-    (FsmTab[curState->curState][sig].ptr)(curState);
-    curState->curState =  FsmTab[curState->curState][sig].nextState;
+	(fsm_tab[cur_state->cur_state][sig].ptr)(cur_state);
+	cur_state->cur_state =  fsm_tab[cur_state->cur_state][sig].nextState;
 }
 
-
-
-
-//输入h-e-l-l-o就显示OK  
-//表格实现
+/*input h-e-l-l-o display OK*/
+/*input right char table*/
 char hello[] = {'h','e','l','l','o'};
 
 int main()
 {
-    char i;
-    unsigned int single;
-    struct FsmCurState cs;
+	char i;
+	unsigned int single;
+	struct fsm_cur_state cs;
 
-    //初始化
-    cs.curState = ST1; 
-    while (1)
-    {
-        i = getchar();
-        if (i == hello[cs.curState])
-        {
-            single = 0;     
-        }
-        else
-        {
-            single = 1;     
-        }
-        FsmDispatch(&cs, single);
-    }
-    return 0;       
+	/*init state*/
+	cs.cur_state = ST1; 
+	while (1) {
+		i = getchar();
+		if (i == hello[cs.cur_state]) {
+			single = 0;     
+		} else {
+			single = 1;     
+		}
+		fsm_dispatch(&cs, single);
+	}
+	return 0;       
 }
